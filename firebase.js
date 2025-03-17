8fa9071f36b5b31a317c8623a3033b51ebb82e8c
Sundar K <107351692+Sundarias@users.noreply.github.com>
2025-03-17 20:10:56 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,3 +8,28 @@
     appId: "1:747309277340:web:7e17d1b99df8c252480d59",
     measurementId: "G-DB505504HF"
   }
+
+  firebase.initializeApp(firebaseConfig);
+
+  const AatralregistrationDB = firebase.database().ref('registrationForm')
+  document.getElementById('registrationForm').addEventListener("submit", submitForm)
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    var name =getElementByVal('name')
+    var email =getElementByVal('email')
+    var department =getElementByVal('department')
+    var otherdept = getElementByVal('other-dept')
+    var phone = getElementByVal('phone')
+    var college =getElementByVal('college')
+    var workshop =getElementByVal('workshop')
+    var totalfee =getElementByVal('total-fee')
+
+    console.log(name, email, department, otherdept, phone, college, workshop, totalfee)
+
+  }
+
+  const getElementByVal = (id) => {
+    return document.getElementById(id).value;
+  }
