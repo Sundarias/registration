2a1133e7736f6ec5b3490b5565cf40defbd90705
Sundar K <107351692+Sundarias@users.noreply.github.com>
2025-03-17 19:51:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB4Anm0VIAdW1T21BFuwO4zmERiHGpKJKQ",
-  authDomain: "aatralregistration.firebaseapp.com",
-  projectId: "aatralregistration",
-  storageBucket: "aatralregistration.firebasestorage.app",
-  messagingSenderId: "747309277340",
-  appId: "1:747309277340:web:7e17d1b99df8c252480d59",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, collection, addDoc };
+  const firebaseConfig = {
+    apiKey: "AIzaSyB4Anm0VIAdW1T21BFuwO4zmERiHGpKJKQ",
+    authDomain: "aatralregistration.firebaseapp.com",
+    databaseURL: "https://aatralregistration-default-rtdb.firebaseio.com",
+    projectId: "aatralregistration",
+    storageBucket: "aatralregistration.firebasestorage.app",
+    messagingSenderId: "747309277340",
+    appId: "1:747309277340:web:7e17d1b99df8c252480d59",
+    measurementId: "G-DB505504HF"
+  }
